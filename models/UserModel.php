b99d7b6cf6976ebd29929b814ea72d615fc92971
PrasadPatel12 <57372001+PrasadPatel12@users.noreply.github.com>
2022-11-05 11:31:16 -0400

    Implemented the CarModel, the CarRentalModel and the UserModel  (#32)
    * Implemented the CarModel
    
    * Implemented the CarRentalModel
    
    * Implemented the UserModel + Updated CarRentalModel
    
    Co-authored-by: PrasadPatel12 <%prasadpatel_12@hotmail.com%>

---
+++ models/UserModel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+class UserModel extends BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // Getting all users
+    public function getAllUsers(): array
+    {
+        $query = 'SELECT * FROM user';
+        return $this->fetchAll($query);
+    }
+
+    // Getting a user by id 
+    public function getUserById(int $user_id): array
+    {
+        $query = 'SELECT * FROM user WHERE user_id = :user_id';
+        return $this->fetchSingle($query, ['user_id' => $user_id]);
+    }
+
+    // Adding a user
+    public function createUser(array $user): int
+    {
+        $query =
+            'INSERT INTO user ' .
+            '(username, password, permission) ' .
+            'VALUES ' .
+            '(:username, :password, :permission)';
+        return $this->execute($query, $user);
+    }
+
+    // Updating a user 
+    public function updateUser(array $user): int
+    {
+        $query = 'UPDATE user SET';
+        foreach ($user as $key => $value) {
+            if ($key != 'user_id') {
+                $query .= " $key = :$key,";
+            }
+        }
+        $query = rtrim($query, ',') . ' WHERE user_id = :user_id';
+        return $this->execute($query, $user);
+    }
+
+    // Deleting a user
+    public function deleteUser(int $user_id): int
+    {
+        $query = 'DELETE FROM user WHERE user_id = :user_id';
+        return $this->execute($query, ['user_id' => $user_id]);
+    }
+}
