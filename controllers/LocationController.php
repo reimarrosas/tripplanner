b1b747001442f58fe91ba73fb971519a42549639
Reimar Rosas <reimar@reimarrosas.me>
2022-11-16 11:22:40 -0500

    (#40) Implemented Weather API, Issue #19
    * Implemented Weather API for the location resource
    
    - Installed GuzzleHttp via composer
    
    - Fully implemented LocationController::getClimate
    
    - Bound the handler to the /locations/{location_id}/climate route
    
    * Formatted the response to include location data
    
    - Added location to the response body
    
    - Extracted the forecast in the weather API response to include in the
      response body

--- controllers/LocationController.php
+++ controllers/LocationController.php
@@ -2,11 +2,13 @@
 
 namespace app\controllers;
 
+use app\config\APIKeys;
 use app\exceptions\HttpUnprocessableEntityException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 use app\models\LocationModel;
+use GuzzleHttp\Client;
 use Slim\Exception\HttpInternalServerErrorException;
 use Slim\Exception\HttpNotFoundException;
 
@@ -40,9 +42,9 @@ class LocationController
     // Route: /locations/{location_id}
     public function getLocation(Request $request, Response $response, array $args): Response
     {
-        $hotel_id = intval($args['location_id']);
-        if ($hotel_id < 1) {
-            throw new HttpUnprocessableEntity($request, 'Location ID is not valid!');
+        $location_id = intval($args['location_id']);
+        if ($location_id < 1) {
+            throw new HttpUnprocessableEntityException($request, 'Location ID is not valid!');
         }
 
         $result = [];
@@ -66,7 +68,7 @@ class LocationController
     {
         $location_id = intval($args['location_id']);
         if ($location_id < 1) {
-            throw new HttpUnprocessableEntity($request, 'Location ID is not valid!');
+            throw new HttpUnprocessableEntityException($request, 'Location ID is not valid!');
         }
 
         $result = [];
@@ -161,6 +163,53 @@ class LocationController
         $response_data = json_encode($arr);
         $response->getBody()->write($response_data);
         return $response->withStatus(201);
+    }
+
+    // Route: /locations/{location_id}/climate
+    public function getClimate(Request $request, Response $response, array $args): Response
+    {
+        $location_id = $args['location_id'] ?? '';
+        $int_location_id = intval($location_id);
+
+        if (!ctype_digit($location_id) || $int_location_id < 1) {
+            throw new HttpUnprocessableEntityException($request, '`location_id` must be an integer > 0');
+        }
+
+        $location = [];
+        try {
+            $location_model = new LocationModel();
+            $location = $location_model->getSingleLocation($int_location_id);
+        } catch (\Throwable $th) {
+            throw new HttpInternalServerErrorException($request, 'Something broke!', $th);
+        }
+
+        if (empty($location)) {
+            throw new HttpNotFoundException($request, "Location $location_id not found!");
+        }
+
+        $client = new Client(['base_uri' => 'http://dataservice.accuweather.com']);
+
+        $api_key = APIKeys::WEATHER;
+        $country = $location['country'];
+        $city = $location['city'];
+
+        $weather = [];
+        try {
+            $res = $client->get("/locations/v1/cities/search?apikey=$api_key&q=$city,$country&metric=true");
+            $body = json_decode($res->getBody());
+            $key = $body[0]->Key;
+            $res = $client->get("/forecasts/v1/daily/1day/$key?apikey=$api_key&metric=true");
+            $weather = json_decode($res->getBody());
+        } catch (\Throwable $th) {
+            throw new HttpInternalServerErrorException($request, $th);
+        }
+
+        $response_body = [
+            'location' => $location,
+            'forecast' => $weather->DailyForecasts
+        ];
+        $response->getBody()->write(json_encode($response_body));
+        return $response;
     }
 
     private function parseLocationFilters(array $query_params): array
